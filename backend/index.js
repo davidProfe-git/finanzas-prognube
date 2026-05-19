cafeeda3b3a439d9d0636dd505508a670e7994a4
David <David@test>
2026-05-19 09:57:31 -0500

    se crea lo basico del backend

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+app.listen(3000,()=>{
+    console.log('cree un servidor web')
+})
+
+
+app.get('/',(req, res)=>{
+    res.send('<h1>Hola soy una app de finanzas</h1>')
+})
